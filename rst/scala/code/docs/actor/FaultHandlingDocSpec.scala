akka/akka

=doc #16910 Fix include sections in fault tolerance docs
* and don't show AkkaSpec in doc code

akka-docs/rst/scala/code/docs/actor/FaultHandlingDocSpec.scala
@@ -6,8 +6,11 @@ package docs.actor
 import language.postfixOps
 
 //#testkit
-import akka.testkit.{ AkkaSpec, ImplicitSender, EventFilter }
-import akka.actor.{ ActorRef, Props, Terminated }
+import akka.testkit.{ TestActors, TestKit, ImplicitSender }
+import org.scalatest.{ WordSpecLike, Matchers, BeforeAndAfterAll }
+import akka.testkit.{ ImplicitSender, EventFilter }
+import akka.actor.{ ActorSystem, ActorRef, Props, Terminated }
+import com.typesafe.config.ConfigFactory
 
 //#testkit
 object FaultHandlingDocSpec {
@@ -16,8 +19,6 @@ object FaultHandlingDocSpec {
   import akka.actor.Actor
 
   //#child
-  //#supervisor
-  //#supervisor
   class Supervisor extends Actor {
     //#strategy
     import akka.actor.OneForOneStrategy
@@ -79,7 +80,6 @@ object FaultHandlingDocSpec {
 
     def receive = Actor.emptyBehavior
   }
-  //#supervisor
 
   //#child
   class Child extends Actor {
@@ -94,7 +94,20 @@ object FaultHandlingDocSpec {
 }
 
 //#testkit
-class FaultHandlingDocSpec extends AkkaSpec with ImplicitSender {
+class FaultHandlingDocSpec(_system: ActorSystem) extends TestKit(_system)
+  with ImplicitSender with WordSpecLike with Matchers with BeforeAndAfterAll {
+
+  def this() = this(ActorSystem("FaultHandlingDocSpec",
+    ConfigFactory.parseString("""
+      akka {
+        loggers = ["akka.testkit.TestEventListener"]
+        loglevel = "WARNING"
+      }
+      """)))
+
+  override def afterAll {
+    TestKit.shutdownActorSystem(system)
+  }
   //#testkit
 
   import FaultHandlingDocSpec._

akka-ja/akka-doc-ja

(cherry picked from commit 23be60395cdea846ef168960cd05e88c6bd29556)
Conflicts:
	akka-docs/rst/scala/code/docs/actor/FaultHandlingDocSpec.scala
	akka-docs/rst/scala/fault-tolerance.rst

rst/scala/code/docs/actor/FaultHandlingDocSpec.scala
@@ -4,12 +4,13 @@
 package docs.actor
 
 import language.postfixOps
+import akka.actor.{ ActorSystem, ActorRef, Props, Terminated }
+import FaultHandlingDocSpec._
 
 //#testkit
-import akka.testkit.{ TestKit, ImplicitSender, EventFilter }
-import akka.actor.{ ActorRef, Props, ActorSystem, Terminated }
-import org.scalatest._
 import com.typesafe.config.{ Config, ConfigFactory }
+import org.scalatest.{ FlatSpecLike, Matchers, BeforeAndAfterAll }
+import akka.testkit.{ TestActors, TestKit, ImplicitSender, EventFilter }
 
 //#testkit
 object FaultHandlingDocSpec {
@@ -97,14 +98,20 @@ object FaultHandlingDocSpec {
       }
   """)
 }
-
-import FaultHandlingDocSpec._
 //#testkit
-class FaultHandlingDocSpec extends TestKit(ActorSystem("FaultHandlingDocSpec", testConf))
-  with FlatSpecLike with BeforeAndAfterAll with ImplicitSender {
+class FaultHandlingDocSpec(_system: ActorSystem) extends TestKit(_system)
+  with ImplicitSender with FlatSpecLike with Matchers with BeforeAndAfterAll {
 
-  override def afterAll() {
-    system.terminate()
+  def this() = this(ActorSystem("FaultHandlingDocSpec",
+    ConfigFactory.parseString("""
+      akka {
+        loggers = ["akka.testkit.TestEventListener"]
+        loglevel = "WARNING"
+      }
+      """)))
+
+  override def afterAll {
+    TestKit.shutdownActorSystem(system)
   }
 
   "A supervisor" must "apply the chosen strategy for its child" in {